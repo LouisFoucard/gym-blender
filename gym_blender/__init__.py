747b925e4770720cf65a6dd0c5defb5064525133
LouisFoucard <foucard@colorado.edu>
2017-01-08 21:47:48 -0800

    conform to gym project srtucture

---
+++ gym_blender/__init__.py
@@ -0,0 +1,6 @@
+from gym.envs.registration import register
+
+register(
+    id='blender-v0',
+    entry_point='gym_blender.envs:blender_env',
+)
---
+++ gym_blender/envs/__init__.py
@@ -0,0 +1,1 @@
+from gym_blender.envs.blender_env import BleEnv
